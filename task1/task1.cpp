cc2fa1857540bbf8c3245194ef05da09a3127031
Falkon353 <martin.mostad@ntebb.no>
2018-01-13 17:46:16 +0100

    started task 2. Having problems with training the tree. Get segmentation foult or assertion foult in run time

--- task1/task1.cpp
+++ task1/task1.cpp
@@ -2,7 +2,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/objdetect/objdetect.hpp"
-#include "hog_visualization.cpp"
+#include "../hog_visualization.cpp"
 #include "iostream"
 
 
